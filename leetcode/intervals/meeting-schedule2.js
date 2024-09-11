fe2f115a0ef15547eb742350b2e23e8deee3acf3
Kunal Indoliya <kunalindoliya@gmail.com>
2024-09-11 15:56:06 +0530

    leetcode

--- leetcode/intervals/meeting-schedule2.js
+++ leetcode/intervals/meeting-schedule2.js
@@ -1,5 +1,29 @@
 /**
  * @read: https://neetcode.io/problems/meeting-schedule-ii
+ *
+ * Meeting Schedule II
+ * Solved
+ * Given an array of meeting time interval objects consisting of start and end times
+ * [[start_1,end_1],[start_2,end_2],...] (start_i < end_i),
+ * find the minimum number of days required to schedule all meetings without any conflicts.
+ *
+ * Example 1:
+ *
+ * Input: intervals = [(0,40),(5,10),(15,20)]
+ *
+ * Output: 2
+ * Explanation:
+ * day1: (0,40)
+ * day2: (5,10),(15,20)
+ *
+ * Example 2:
+ *
+ * Input: intervals = [(4,9)]
+ *
+ * Output: 1
+ * Note:
+ *
+ * (0,8),(8,10) is not considered a conflict at 8
  */
 
 /**
